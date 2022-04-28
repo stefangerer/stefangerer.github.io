aa27b473a64c30800a22b8f8c9f378ee6bc15ba5
Stefan Angerer <angerer.st@gmail.com>
2022-04-28 15:39:52 +0200

    unnötiges gelöscht

--- nz/main.js
+++ nz/main.js
@@ -6,9 +6,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-43.512, 170.105]).addTo(map)
-    .bindPopup('Foc-Gletscher.<br> Wo Eis noch Eis ist.')
-    .openPopup();
+
 
     for (let etappe of ETAPPEN) {
         let popup = `
